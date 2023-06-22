38130b11f8ff51959a9e4b0ef529dd56c0903b01
Eduardo_Lira <135743538+Edulira14@users.noreply.github.com>
2023-06-22 16:34:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula07jogos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random sorteador = new Random();    
+            
+            int numero;
+            int sorteio = sorteador.Next(1,11);
+
+            Console.WriteLine("Bem-vindo a Conradito Jogos \n");
+            Console.WriteLine("Adivinhe o numero de 1 até 10");
+            Console.WriteLine("Digite o número:");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero > 10 || numero < 1 ) 
+            {
+                Console.WriteLine("Digite um valor valido");
+                Main(null);
+                return;
+            }
+            if(numero == sorteio)
+            {
+                Console.WriteLine("Parabens, você acertou o número e ganhou!");
+            }
+            else
+            {
+                Console.WriteLine("Que pena, você errou! Tente novamente...");
+                Main(null);
+                return;
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
